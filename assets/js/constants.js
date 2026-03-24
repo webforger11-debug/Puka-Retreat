bda5176fdf0dab3c49ed8acffdede9218e2dbe69
webforger11-debug <webforger11@gmail.com>
2026-03-25 00:27:50 +0100

    Delete assets directory

--- assets/js/constants.js
+++
@@ -1,9 +0,0 @@
-export const APP_CONFIG = Object.freeze({
-  propertyName: "Puka Retreat",
-  storageKey: "puka_retreat_invoices_v1",
-  authKey: "puka_retreat_auth_v1",
-  seedDataFile: "data/invoices.json",
-  adminUsername: "enea",
-  adminPassword: "Miqo123-",
-  currency: "EUR"
-});
